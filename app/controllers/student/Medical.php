16d397acbe77e79337cde058bf872bb551f94027
AmarAbdullaa <amarlareef01@gmail.com>
2025-03-07 19:32:52 +0530

    Made insert for Medica

--- app/controllers/student/Medical.php
+++ app/controllers/student/Medical.php
@@ -2,9 +2,30 @@
 class Medical extends Controller{
    public function index(){
 
-        $this->view('student/medical');
-    }
+        if($_SERVER['REQUEST_METHOD']=='POST')
+        {
+            //Get DATA
 
-   
+            $data = [
+                'Name' => $_POST['Name'],
+                'RegistrationID' => $_POST['RegistrationID'],
+                'ReasonForMedical' => $_POST['ReasonForMedical'],
+                'TimePeriod' => $_POST['TimePeriod'],  
+            ];
+
+            $medicalrequest = new MedicalRequest();
+
+            if($medicalrequest->validate($data))
+            {
+                $isInserted = $medicalrequest->insert($data);
+
+                if (!$isInserted){
+                    redirect('student/Addmedical');
+                }
+            }
+        } else {
+            $this->view('student/medical'); 
+        }
+    }
 
 }
--- app/controllers/student/Request.php
+++ app/controllers/student/Request.php
@@ -1,10 +1,31 @@
-<?php
+<?php 
 class Request extends Controller{
-   public function index(){
+    public function index(){
 
-        $this->view('student/request');
+        if($_SERVER['REQUEST_METHOD']=='POST')
+        {
+            //Get DATA
+
+            $data =[
+                'Name' => $_POST['Name'],
+                'RegistrationID' => $_POST['RegistrationID'],
+                'HaveYouPlayedBefore'=>$_POST['HaveYouPlayedBefore'],
+                'ReasonForJoining'=>$_POST['ReasonForJoining'],
+            ];
+
+            $teamrequest = new Teamrequest();
+
+            if($teamrequest->validate($data))
+            {
+                $isInserted = $teamrequest->insert($data);
+
+                if(!$isInserted){
+                    redirect('student/Addteamrequest');
+                }
+            }
+        }
+        else{
+            $this->view('student/request');
+        }
     }
-
-   
-
 }
